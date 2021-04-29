019afd69dc6b48a395890109c20b0f5236b917ff
Lena <elenagolubickaya@gmail.com>
2021-04-29 13:00:35 +0300

    4. Расстояние между точками в трехмерном пространстве. [#441753]

--- src/main/java/ru/job4j/oop/Point.java
+++ src/main/java/ru/job4j/oop/Point.java
@@ -3,23 +3,38 @@ package ru.job4j.oop;
 import static java.lang.StrictMath.pow;
 import static java.lang.StrictMath.sqrt;
 
-    public class Point {
-        private int x;
-        private int y;
+public class Point {
+    private int x;
+    private int y;
+    private int z;
 
-        public Point(int first, int second) {
-            this.x = first;
-            this.y = second;
-        }
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
 
-        public double distance(Point that) {
-            return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
-        }
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 
-        public static void main(String[] args) {
-            Point a = new Point(0, 0);
-            Point b = new Point(0, 2);
-            double dist = a.distance(b);
-            System.out.println(dist);
-        }
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
+
+    public static void main(String[] args) {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        Point c = new Point(0, 2, 0);
+        Point d = new Point(0, 0, 2);
+        double dist1 = a.distance(b);
+        System.out.println(dist1);
+        double dist2 = c.distance(d);
+        System.out.println(dist2);
+    }
 }
--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -6,13 +6,23 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.closeTo;
 
-public class TriangleTest{
+public class TriangleTest {
     @Test
     public void area() {
         Point a = new Point(0, 0);
         Point b = new Point(4, 0);
         Point c = new Point(0, 4);
         Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, closeTo(8, 0.001));
+    }
+
+    @Test
+    public void area1() {
+        Point x = new Point(0, 0, 0);
+        Point y = new Point(4, 0, 0);
+        Point z = new Point(0, 4, 0);
+        Triangle triangle = new Triangle(x, y, z);
         double rsl = triangle.area();
         assertThat(rsl, closeTo(8, 0.001));
     }
