c64188f64ba10fb84349fea7fcca8f1b46c8bac2
Lena <elenagolubickaya@gmail.com>
2021-05-16 12:27:48 +0300

    2. Модель данных. [#441775]

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -7,6 +7,10 @@ public class College {
         student.setSurname("Ivanov");
         student.setGroup(2);
         student.setReceiptDate("12.06.2012");
-        System.out.println("Студент " + student.getName() + " " + student.getSurname() + "\n" + "группа " + student.getGroup() + "\n" + "дата поступления " + student.getReceiptDate());
+        System.out.println(
+                "Студент " + student.getName() + " " + student.getSurname() + "\n" +
+                "группа " + student.getGroup() + "\n" +
+                "дата поступления " + student.getReceiptDate()
+        );
     }
 }
