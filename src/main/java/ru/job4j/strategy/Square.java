97805c32d54b4aae1b3756229192fdc4a6cb4bea
Lena <elenagolubickaya@gmail.com>
2021-06-10 17:31:20 +0300

    7. Шаблон проектирования - Стратегия [#786]

--- src/main/java/ru/job4j/strategy/Square.java
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -1,6 +1,7 @@
 package ru.job4j.strategy;
 
 public class Square implements Shape {
+
     @Override
     public String draw() {
         String ln = System.lineSeparator();
@@ -9,9 +10,5 @@ public class Square implements Shape {
                 + "|         |" + ln
                 + "|         |" + ln
                 +  "---------" + ln;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new Square().draw());
     }
 }
--- src/main/java/ru/job4j/strategy/Triangle.java
+++ src/main/java/ru/job4j/strategy/Triangle.java
@@ -1,15 +1,12 @@
 package ru.job4j.strategy;
 
 public class Triangle implements Shape {
+
     @Override
     public String draw() {
         String ln = System.lineSeparator();
-        return   "   ^    " + ln
+        return "   ^    " + ln
                 + " /   \\ " + ln
                 + "/_____\\" + ln;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new Triangle().draw());
     }
 }
