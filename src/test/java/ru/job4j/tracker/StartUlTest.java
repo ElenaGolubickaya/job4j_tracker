5f60e1a97e2a8096f8b497732442bad1a75ca11e
Lena <elenagolubickaya@gmail.com>
2021-06-28 18:26:33 +0300

    9.2. Тесты вывода на консоль [#33585]

--- src/test/java/ru/job4j/tracker/StartUlTest.java
+++ src/test/java/ru/job4j/tracker/StartUlTest.java
@@ -97,8 +97,6 @@ public class StartUlTest {
                 "Menu." + ln
                         + "0. Find item by id" + ln
                         + "1. Exit" + ln
-                        + "Enter id:" + ln
-                        + item + ln
                         + "Menu." + ln
                         + "0. Find item by id" + ln
                         + "1. Exit" + ln
@@ -125,7 +123,7 @@ public class StartUlTest {
                         + "1. Exit" + ln
                         + item1 + ln
                         + "Menu." + ln
-                        + "0. Find items by mane" + ln
+                        + "0. Find items by name" + ln
                         + "1. Exit" + ln
         ));
     }
