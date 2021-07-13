7b6c4182e765def8954296c8d25f3eb639a0eb7b
Lena <elenagolubickaya@gmail.com>
2021-07-13 10:43:49 +0300

    0.4. Пользовательские исключения. [#219367]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -2,13 +2,12 @@ package ru.job4j.ex;
 
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        int rsl = -1;
         for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
-                rsl = i;
                 return i;
             }
         }
+
         throw new ElementNotFoundException("there is no such element in the array");
     }
 
