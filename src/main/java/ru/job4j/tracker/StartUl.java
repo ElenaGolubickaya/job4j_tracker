63fbcb07d3ae819bc230a45640342f1c2ec73451
Lena <elenagolubickaya@gmail.com>
2021-07-15 16:04:33 +0300

    1. Обеспечить бесперебойную работу приложения Tracker [#789]

--- src/main/java/ru/job4j/tracker/StartUl.java
+++ src/main/java/ru/job4j/tracker/StartUl.java
@@ -15,6 +15,10 @@ public class StartUl {
         while (run) {
             this.showMenu(actions);
             int select = input.askInt("Select: ");
+            if (select < 0 || select >= actions.length) {
+                out.println("Wrong input, you can select: 0 .. " + (actions.length - 1));
+                continue;
+            }
             UserAction action = actions[select];
             run = action.execute(input, tracker);
         }
@@ -23,13 +27,13 @@ public class StartUl {
     private void showMenu(UserAction[] actions) {
         out.println("Menu.");
         for (int index = 0; index < actions.length; index++) {
-            out.println(index + "." + actions[index].name());
+            out.println(index + ". " + actions[index].name());
         }
     }
 
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
-        Input input = new ConsoleInput();
+        Input input = new ValidateInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new CreateAction(output), new FindAllAction(output), new ReplaceAction(output),
