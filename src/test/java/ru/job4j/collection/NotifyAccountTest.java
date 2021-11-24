ab7d46046c481eb95eb064d342844e583e97ad63
Lena <elenagolubickaya@gmail.com>
2021-11-24 22:15:51 +0300

    Модели данных и HashSet

--- src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -1,11 +1,9 @@
 package ru.job4j.collection;
 
 import org.junit.Test;
-
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
