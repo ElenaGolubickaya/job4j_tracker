1c20d76aac654543ac65fc339e06258d05b932b7
Lena <elenagolubickaya@gmail.com>
2021-11-30 15:24:19 +0300

    Map, HashMap

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,7 +6,7 @@ public class UsageMap {
         HashMap<String, String> map = new HashMap<>();
         map.put("parsentev@yandex.ru", "Arsentev Petr Sergeevich");
         map.put("info1@yandex.ru", "Golubitskaya Elena");
-        map.put("info2@yandex.ru", "Golubitskaya Elena ");
+        map.put("info2@yandex.ru", "Golubitskaya Elena");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
